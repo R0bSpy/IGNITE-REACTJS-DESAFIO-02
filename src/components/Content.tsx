2e6cf09d0f987912a82115baf7c01c3860c1b2f2
Lipe <robson.santos3@estudante.ifms.edu.br>
2022-01-18 15:14:14 -0300

    Finish challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,18 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+import '../styles/Content.scss';
+
+interface IMovieProps {
+  movies: MovieProps[]
+}
+
+export function Content({ movies }: IMovieProps) {
+  return (<main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>)
 }
